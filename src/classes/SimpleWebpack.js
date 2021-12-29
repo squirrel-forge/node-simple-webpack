737ead9d0838394903da7e820c537437af565ae1
dasiux <me@siux.info>
2021-12-29 05:05:45 +0100

    prototype

---
+++ src/classes/SimpleWebpack.js
@@ -0,0 +1,384 @@
+/**
+ * Requires
+ */
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const ESLintPlugin = require( 'eslint-webpack-plugin' );
+const { Exception, Timer, FsInterface, isPojo } = require( '../../../node-util/index.js' );
+
+/**
+ * SimpleWebpack exception
+ * @class
+ */
+class SimpleWebpackException extends Exception {}
+
+/**
+ * @typedef {Object} SimpleWebpackRunOptions
+ * @property {null|'index'|'recursive'} source - Source read mode
+ * @property {null|string} name - Entry name for index and recursive mode bundling
+ * @property {null|{exclude:RegExp,extensions:RegExp}} read - Source read options
+ * @property {null|string} public - Webpack publicPath option
+ * @property {null|Object|Function} extend - Extend the generated config
+ */
+
+/**
+ * SimpleWebpack class
+ * @class
+ */
+class SimpleWebpack {
+
+    /**
+     * Constructor
+     * @constructor
+     * @param {null|console} cfx - Console or alike object
+     */
+    constructor( cfx = null ) {
+
+        /**
+         * Timer
+         * @public
+         * @property
+         * @type {Timer}
+         */
+        this.timer = new Timer();
+
+        /**
+         * Console alike reporting object
+         * @protected
+         * @property
+         * @type {console|null}
+         */
+        this._cfx = cfx;
+
+        /**
+         * Strict mode
+         * @public
+         * @property
+         * @type {boolean}
+         */
+        this.strict = true;
+
+        /**
+         * Verbose mode
+         * Outputs the full stack of nonfatal exceptions
+         * @public
+         * @property
+         * @type {boolean}
+         */
+        this.verbose = true;
+
+        /**
+         * Production or dev mode
+         * @type {boolean}
+         */
+        this.production = process.env.NODE_ENV === 'production';
+    }
+
+    /**
+     * Parse exception for output
+     * @protected
+     * @param {string|Error|Exception} msg - Message or exception instance
+     * @param {boolean} noTrace - Do not output trace since it is internal
+     * @return {string} - Exception output
+     */
+    _exceptionAsOutput( msg, noTrace = false ) {
+
+        // We check if its an exception, all other errors will be sent to output unmodified
+        if ( msg instanceof Error ) {
+            if ( this.verbose && !noTrace ) {
+
+                // In verbose we want to whole stack
+                return msg.stack;
+            } else {
+
+                // In normal mode we just send the short string representation without the stack
+                return msg + '';
+            }
+        }
+        return msg;
+    }
+
+    /**
+     * Error output
+     *  Throw in strict mode or always true
+     *  Notify in normal mode
+     *  Show full trace in verbose mode
+     * @public
+     * @param {string|Error|Exception} msg - Message or exception instance
+     * @param {boolean} always - Fatal error, always throw
+     * @throws {Exception}
+     * @return {void}
+     */
+    error( msg, always = false ) {
+
+        // In strict mode we always throw
+        if ( always || this.strict ) {
+            throw msg;
+        }
+
+        // If we are not silent and we have a fitting error logger
+        if ( this._cfx && typeof this._cfx.error === 'function' ) {
+            this._cfx.error( this._exceptionAsOutput( msg ) );
+        }
+    }
+
+    /**
+     * Parse options
+     * @protected
+     * @param {Object|SimpleWebpackRunOptions} options - Options object
+     * @return {SimpleWebpackRunOptions} - Parsed options
+     */
+    _parseOptions( options ) {
+        if ( !isPojo( options ) ) {
+            return {};
+        }
+        return options;
+    }
+
+    /**
+     * Resolve source
+     * @protected
+     * @param {string} source - Source path
+     * @param {SimpleWebpackRunOptions} options - Options object
+     * @return {Promise<{root: string, files: string[], source, resolved: string}>} - Source object
+     */
+    async _resolveSource( source, options ) {
+
+        // Resolve source
+        const resolved = path.resolve( source );
+
+        // Require valid source
+        const source_exists = await FsInterface.exists( resolved );
+        if ( !source_exists ) {
+            throw new SimpleWebpackException( 'Source not found: ' + resolved );
+        }
+
+        // Convert to array for processing
+        let files = [ resolved ], root = resolved;
+
+        // Fetch files if source is a directory
+        if ( FsInterface.isDir( resolved ) ) {
+
+            // Index mode uses all index.js files as entry points, recursive uses custom options
+            if ( options.source && [ 'index', 'recursive' ].includes( options.source ) ) {
+                if ( !options.read ) {
+
+                    // In recursive mode we expect custom options
+                    if ( options.source === 'recursive' ) {
+                        throw new SimpleWebpackException( 'Source mode recursive requires a read filter' );
+                    }
+
+                    // Set the index mode options
+                    options.read = { exclude : /^(?!.*index).*\.js$/, extensions : /\.js/ };
+                }
+                files = FsInterface.fileList( resolved, options.read );
+            } else {
+
+                // Set the default mode options
+                if ( !options.read ) {
+                    options.read = { extensions : /\.js/ };
+                }
+                files = await FsInterface.files( resolved, options.read );
+            }
+
+            // Require file results
+            if ( !files.length ) {
+                throw new SimpleWebpackException( 'Source is empty: ' + resolved );
+            }
+        } else {
+            root = path.dirname( resolved );
+        }
+
+        // Map to root relative paths
+        files = files.map( ( file ) => {
+            return './' + FsInterface.relative2root( file, root );
+        } );
+
+        return { root, source, resolved, files };
+    }
+
+    /**
+     * Resolve target
+     * @protected
+     * @param {string} target - Target source
+     * @return {Promise<{created: null, exists: boolean, target, resolved: string}>} - Target object
+     */
+    async _resolveTarget( target ) {
+
+        // Resolve target
+        const resolved = path.resolve( target );
+
+        // Attempt create
+        let created = null, exists = await FsInterface.exists( resolved );
+        if ( !exists ) {
+            created = await FsInterface.dir( resolved );
+            exists = true;
+        }
+
+        // Check for directory if not created
+        if ( !created && !FsInterface.isDir( resolved ) ) {
+            throw new SimpleWebpackException( 'Target must be a directory: ' + resolved );
+        }
+        return { target, resolved, exists, created };
+    }
+
+    /**
+     * Generate webpack config
+     * @param {Object} source - Source object
+     * @param {Object} target - Target object
+     * @param {Object|SimpleWebpackRunOptions} options - Options object
+     * @return {Promise<Object>} - Setup object
+     */
+    async generateConfig( source, target, options ) {
+        options = this._parseOptions( options );
+        source = await this._resolveSource( source, options );
+        target = await this._resolveTarget( target, options );
+        const config = {
+            mode : this.production ? 'production' : 'development',
+            context : source.root,
+            entry : this._getEntry( source, options ),
+            output : this._getOutput( target, options ),
+            plugins : [ new ESLintPlugin( { fix : true } ) ],
+            module : {
+                rules : [
+                    {
+                        test : /\.m?js$/,
+                        use : {
+                            loader : 'babel-loader',
+                            options : { presets : [ '@babel/preset-env' ] }
+                        }
+                    }
+                ]
+            },
+            optimization : { minimize : this.production },
+        };
+
+        // Custom extend
+        if ( typeof options.extend === 'function' ) {
+            options.extend( config, source, target, this );
+        } else if ( isPojo( options.extend ) ) {
+            Object.assign( config, options.extend );
+        }
+        return { source, target, options, config };
+    }
+
+    /**
+     * Get entry data
+     * @protected
+     * @param {Object} source - Source object
+     * @param {Object|SimpleWebpackRunOptions} options - Option object
+     * @return {Object} - Entry object
+     */
+    _getEntry( source, options ) {
+        const entry = {};
+
+        // Combine into one bundle entry
+        if ( typeof options.name === 'string' ) {
+            if ( !options.name.length ) {
+                throw new SimpleWebpackException( 'Entry name must be a none empty string' );
+            }
+            if ( options.prepend instanceof Array ) {
+                source.files.unshift( ...options.prepend );
+            }
+            entry[ options.name ] = source.files;
+        } else {
+
+            // Add individual entries
+            for ( let i = 0; i < source.files.length; i++ ) {
+                const file = source.files[ i ];
+                const name = path.basename( file, path.extname( file ) );
+                entry[ name ] = file;
+            }
+        }
+        return entry;
+    }
+
+    /**
+     * Get output data
+     * @protected
+     * @param {Object} target -Target object
+     * @param {Object|SimpleWebpackRunOptions} options - Options object
+     * @return {Object} - Output object
+     */
+    _getOutput( target, options ) {
+        const output = {
+            filename : '[name]' + ( this.production ? '.min' : '' ) + '.js',
+            path : target.resolved,
+        };
+        if ( options.public ) {
+            output.publicPath = options.public;
+        }
+        return output;
+    }
+
+    /**
+     * Run webpack
+     * @protected
+     * @param {Object} config - Webpack config
+     * @param {null|Object} statsOptions - Webpack stats options for errors
+     * @return {Promise<Object|SimpleWebpackException>} - Stats object or exception
+     */
+    _compile( config, statsOptions = null ) {
+        return new Promise( ( resolve ) => {
+            webpack( config, ( err, stats ) => {
+                if ( err || stats.hasErrors() ) {
+                    if ( err ) {
+                        resolve( new SimpleWebpackException( 'Compile error', err ) );
+                    } else if ( stats.hasErrors() ) {
+                        resolve( new SimpleWebpackException( 'Compile error',
+                            'WebpackStatsInfo:\n' + stats.toString( statsOptions || {
+                                all : false,
+                                colors : true,
+                                errors : true,
+                                errorDetails : true,
+                                warnings : true,
+                            } ) ) );
+                    } else {
+                        resolve( new SimpleWebpackException( 'Unknown compile error',
+                            'WebpackStatsInfo:\n' + stats.toString( statsOptions || { all : true } ) ) );
+                    }
+                } else {
+                    resolve( stats );
+                }
+            } );
+        } );
+    }
+
+    /**
+     * Run build
+     * @param {string} source - Source path
+     * @param {string} target - Target path
+     * @param {null|Object} options - Options object
+     * @return {Promise<{compiled: number, sources: number, files: *[], dirs: {created: *[], failed: *[]}, time: null}>} - Stats
+     */
+    async run( source, target, options = null ) {
+        this.timer.start( 'total-run' );
+        const setup = await this.generateConfig( source, target, options );
+        const stats = {
+            source : setup.source,
+            target : setup.target,
+            options : setup.options,
+            config : setup.config,
+            webpack : null,
+            time : null,
+        };
+
+        // Run compile
+        const wps = await this._compile( setup.config );
+
+        // Notify error
+        if ( wps instanceof Error ) {
+            this.error( wps );
+        } else if ( wps ) {
+            stats.webpack = wps;
+        }
+
+        stats.time = this.timer.measure( 'total-run' );
+
+        return stats;
+    }
+}
+
+// Export Exception as static property constructor
+SimpleWebpack.SimpleWebpackException = SimpleWebpackException;
+module.exports = SimpleWebpack;
---
+++ src/cli.js
@@ -0,0 +1,301 @@
+/**
+ * Requires
+ */
+const path = require( 'path' );
+const { cfx } = require( '@squirrel-forge/node-cfx' );
+const { CliInput, Progress, Timer, leadingZeros, StatsDisplay, convertBytes } = require( '@squirrel-forge/node-util' );
+const SimpleWebpack = require( './classes/SimpleWebpack' );
+
+/**
+ * Simple webpack cli application
+ * @return {Promise<void>} - Possibly throws errors in strict mode
+ */
+module.exports = async function cli() {
+
+    // Timer
+    const timer = new Timer();
+
+    // Input
+    const input = new CliInput( cfx );
+
+    // Main arguments
+    let source = input.arg( 0 ) || '',
+        target = input.arg( 1 ) || '';
+    if ( !target.length ) {
+        target = source;
+        source = '';
+    }
+
+    // Cli application options
+    const options = input.getFlagsOptions( {
+
+        // Show version
+        version : [ '-v', '--version', false, true ],
+
+        // Show more output
+        stats : [ '-s', '--stats', false, true ],
+
+        // Show more output
+        verbose : [ '-i', '--verbose', false, true ],
+
+        // Force development mode
+        dev : [ '-d', '--development', false, true ],
+
+        // Force production mode
+        prod : [ '-p', '--production', false, true ],
+
+        // Bundle all sources
+        bundle : [ '-b', '--bundle', false, true ],
+
+        // Bundle name
+        name : [ '-n', '--name', null, false ],
+
+        // Index source mode
+        index : [ ' ', '--index', false, true ],
+
+        // Color limits
+        colors : [ '-w', '--colors', '', false ],
+
+        // Do not break on any error, disables the default strict if set
+        loose : [ '-u', '--loose', null, true ],
+
+    } );
+
+    // Cannot force dev and prod mode
+    if ( options.dev && options.prod ) {
+        cfx.error( 'Cannot force production and development mode at the same time' );
+        process.exit( 1 );
+    }
+
+    // Show version
+    if ( options.version ) {
+        const install_dir = path.resolve( __dirname, '../' );
+        let pkg;
+        try {
+            pkg = require( path.join( install_dir, 'package.json' ) );
+        } catch ( e ) {
+            cfx.error( e );
+            process.exit( 1 );
+        }
+        cfx.log( pkg.name + '@' + pkg.version );
+        cfx.info( '- Installed at: ' + install_dir );
+        process.exit( 0 );
+    }
+
+    // Init application
+    const swp = new SimpleWebpack( cfx );
+    const swpOptions = {};
+
+    // Set options
+    if ( options.loose ) {
+        swp.strict = false;
+    }
+    swp.verbose = options.verbose;
+    if ( options.prod ) {
+        swp.production = true;
+    } else if ( options.dev ) {
+        swp.production = false;
+    }
+
+    // Index source mode
+    if ( options.index ) {
+        swpOptions.source = 'index';
+    }
+
+    // Bundle options
+    if ( options.bundle ) {
+        swpOptions.name = options.name && options.name.length ? options.name : 'bundle';
+
+        // Prepend options
+        if ( options.prepend ) {
+            const prepend = options.prepend.split( ',' )
+                .filter( ( v ) => { return !!v.length; } );
+            if ( prepend.length ) {
+                swpOptions.prepend = prepend;
+            }
+        }
+    }
+
+    // Color warning option must be an Array
+    if ( !( options.colors instanceof Array ) ) {
+        options.colors = options.colors.split( ',' )
+            .filter( ( v ) => { return !!v.length; } )
+            .map( ( x ) => { return parseInt( x, 10 ) * 1024; } );
+    }
+
+    // Use default color if not enough defined
+    if ( options.colors.length !== 3 ) {
+
+        // Notify user if something is defined
+        if ( options.verbose && options.colors.length ) {
+            cfx.info( 'Using default coloring, [fwhite]-c[fcyan] or [fwhite]--colors'
+                + ' [fcyan]must contain 3 incrementing kib limit integers' );
+        }
+
+        // Set default coloring limits
+        options.colors = [ 200 * 1024, 400 * 1024, 500 * 1024 ];
+    }
+    const [ mark_green, mark_yellow, mark_red ] = options.colors;
+
+    // Notify strict mode
+    if ( swp.strict && swp.verbose ) {
+        cfx.warn( 'Running in strict mode!' );
+    }
+
+    // Init progress spinner, stats and count
+    const spinner = new Progress();
+    const stDi = new StatsDisplay( cfx );
+
+    /**
+     * Get file stats data as array
+     * @param {Object} asset - Webpack stats asset object
+     * @return {Array<string>} - Styled file stats parts
+     */
+    const getFileStats = ( asset ) => {
+        const output = [];
+
+        // Source chunks
+        output.push( '- ' + stDi.show( [ leadingZeros( asset.chunkNames.join( ', ' ), 14, ' ', true ), 'none' ], true ) );
+
+        // Make extra stats output
+        if ( options.stats ) {
+
+            // Begin bracket block
+            output.push( '[fred][[re]' );
+
+            // File size
+            if ( asset.size ) {
+                output.push( stDi.show( 'Output:', true ) );
+
+                // Show output size
+                let size_color = 'none';
+                if ( asset.size <= mark_green ) {
+                    size_color = 'valid';
+                } else if ( asset.size <= mark_yellow ) {
+                    size_color = 'notice';
+                } else if ( asset.size > mark_red ) {
+                    size_color = 'error';
+                }
+                output.push( stDi.show( [ leadingZeros( convertBytes( asset.size ), 11, ' ' ) + ' ', size_color ], true ) );
+            }
+
+            // End bracket block
+            output.push( '[fred]][re]' );
+        } else {
+            output.push( '[fred]>[re]' );
+        }
+
+        // Output file
+        output.push( stDi.show( [ './' + asset.name, 'path' ], true ) );
+
+        return output;
+    };
+
+    /**
+     * Convert ms to hr time format
+     * @param {number} ms - Milliseconds
+     * @return {Array<number>} - Hrtime format
+     */
+    const ms2hrtime = ( ms ) => {
+        const seconds = Math.floor( ms / 1000 );
+        return [ seconds, ( ms - seconds * 1000 ) * 1000000 ];
+    };
+
+    // Begin processing
+    swp.strict && spinner.start( 'Building... ' );
+    let stats;
+    try {
+
+        // Run render, process and write
+        stats = await swp.run( source, target, swpOptions );
+    } catch ( e ) {
+        swp.strict && spinner.stop();
+
+        // Generate cleaner exception output only full trace on verbose
+        const error = new SimpleWebpack.SimpleWebpackException( 'Something went wrong', e );
+        swp.error( swp._exceptionAsOutput( error, !swp.verbose ) );
+        process.exit( 1 );
+    }
+
+    // If we did not crash, stop spinner and inform user
+    swp.strict && spinner.stop();
+
+    // Output result info
+    if ( !stats.webpack || typeof stats.webpack.toJson !== 'function' ) {
+
+        // Warn user since there were stats
+        cfx.error( 'simple-webpack failed for some unknown reason!' );
+        if ( swp.verbose ) {
+            cfx.info( 'Completed after [fwhite]' + timer.end( 'construct' ) );
+        }
+        process.exit( 1 );
+    } else {
+        const info = stats.webpack.toJson( { all : false, assets : true, warnings : true } );
+        if ( swp.verbose ) {
+            if ( stats.webpack.hasWarnings() ) {
+                cfx.error( 'Webpack ' + stats.webpack.toString( { all : false, warnings : true } ) );
+                cfx.success( 'Output files:' );
+            }
+            for ( let i = 0; i < info.assets.length; i++ ) {
+                cfx.info( getFileStats( info.assets[ i ] ).join( ' ' ) );
+            }
+        }
+        cfx.success( 'simple-webpack wrote [ ' + info.assets.length
+            + ' ] file' + ( info.assets.length === 1 ? '' : 's' )
+            + ( swp.verbose ? ''
+                : ' with [' + info.warnings.length + '] warning' + ( info.warnings.length === 1 ? '' : 's' ) )
+            + ' in ' + timer.end( 'construct' ) );
+    }
+
+    // Generate stats on request only
+    if ( options.stats ) {
+        const info = stats.webpack.toJson( {
+            all : false,
+            assets : true,
+            colors : true,
+            entrypoints : true,
+            hash : true,
+            timings : true,
+            warnings : true,
+        } );
+
+        const sources_count = Object.keys( info.entrypoints ).length;
+        const so = {
+            Overview : {
+                Build : info.hash,
+                Files : [ [ 'Sources:', sources_count ], 'asline' ],
+                Time : [ stats.time, 'time' ],
+                Webpack : [ ms2hrtime( info.time ), 'time' ],
+            },
+        };
+
+        if ( sources_count !== info.assets.length ) {
+            so.Overview.Files[ 0 ].push( 'Outputs:' );
+            so.Overview.Files[ 0 ].push( info.assets.length );
+        }
+
+        if ( !options.verbose ) {
+            const files_prop = 'Asset output details';
+            for ( let i = 0; i < info.assets.length; i++ ) {
+                if ( !so[ files_prop ] ) so[ files_prop ] = [];
+                so[ files_prop ].push( [ '[fcyan]' + getFileStats( info.assets[ i ] ).join( ' ' ), 'none' ] );
+            }
+            so[ files_prop ].push( '' );
+        }
+
+        if ( !swp.verbose && stats.webpack.hasWarnings() ) {
+            let warnings = stats.webpack.toString( { all : false, warnings : true } ).trim();
+            const prefix = warnings.indexOf( '\n' );
+            if ( prefix < 25 ) {
+                warnings = warnings.substr( prefix );
+            }
+            so.Warnings = warnings;
+        }
+
+        // Show generated stats
+        stDi.display( so );
+    }
+
+    // End application
+    process.exit( 0 );
+};
